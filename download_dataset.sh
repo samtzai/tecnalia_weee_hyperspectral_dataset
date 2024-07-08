22a78f4d02a96f6824639c3e9ece3c5ca56061c0
samtzai <artzai.picon@ehu.eus>
2024-07-08 15:30:00 +0200

    Update download_dataset.sh

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,2 +1,2 @@
-curl -o dataset.zip https://zenodo.org/records/12565131/files/tecnalia_weee_hyperspectral_dataset_v1.0.zip?download=1
+curl -o dataset.zip https://zenodo.org/records/12565131/files/dataset.zip?download=1
 unzip dataset.zip -d dataset
